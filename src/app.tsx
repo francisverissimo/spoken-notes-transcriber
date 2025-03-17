77861f37c7c318f29c9dbd587b089bd46dbfe72e
francissverissimo <francissv97@gmail.com>
2025-03-16 21:49:30 -0400

    feat: project change name

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,6 @@
 import { ChangeEvent, useState } from 'react'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
-import logo from './assets/logo-nlw-expert.svg'
 
 interface Note {
   id: string
@@ -69,18 +68,23 @@ export function App() {
 
   return (
     <div className="mx-auto my-12 max-w-6xl space-y-6 px-5">
-      <img src={logo} alt="NLW Expert" />
+      <strong className="inline-block text-2xl font-extralight italic text-slate-500">
+        spoken notes transcriber
+      </strong>
 
-      <form className="w-full">
-        <input
-          type="text"
-          placeholder="Busque em suas notas..."
-          onChange={handleSearch}
-          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
-        />
-      </form>
-
-      <div className="h-px bg-slate-700" />
+      {notes.length ? (
+        <>
+          <form className="w-full">
+            <input
+              type="text"
+              placeholder="Busque em suas notas..."
+              onChange={handleSearch}
+              className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+            />
+          </form>
+          <div className="h-px bg-slate-700" />
+        </>
+      ) : null}
 
       <div className="grid auto-rows-[250px] grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
         <NewNoteCard handleCreateNote={handleCreateNote} />
