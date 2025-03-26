af437d2dbf4883bce0ca8f307b9c448222f3e351
francissverissimo <francissv97@gmail.com>
2025-03-26 01:48:28 -0400

    style: just css adjustments

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,12 +1,12 @@
 import { ComponentProps } from 'react'
 
 interface ButtonProps extends ComponentProps<'button'> {
-  color?: 'slate-300' | 'lime-400' | 'cyan-500' | 'red-500'
+  color: 'slate-300' | 'lime-400' | 'cyan-500' | 'red-500'
 }
 
 export function Button({ color, ...props }: ButtonProps) {
   const { value, type, children, className } = props
-  const standartClasses = "group flex w-full items-center justify-center rounded-full bg-slate-800 py-4 text-center text-lg font-medium outline-none ring ring-transparent focus-within:data-[color=cyan-500]:ring-cyan-500 focus-within:data-[color=lime-400]:ring-lime-400 focus-within:data-[color=red-500]:ring-red-500 focus-within:data-[color=slate-300]:ring-slate-300"
+  const standartClasses = "group flex w-full items-center justify-center rounded-4xl bg-slate-800 py-4 text-center text-lg font-medium outline-none ring ring-transparent focus-within:data-[color=cyan-500]:ring-cyan-500 focus-within:data-[color=lime-400]:ring-lime-400 focus-within:data-[color=red-500]:ring-red-500 focus-within:data-[color=slate-300]:ring-slate-300"
   const classnames = className ? standartClasses.concat(` ${className}`) : standartClasses
 
   return (
--- src/view/note.tsx
+++ src/view/note.tsx
@@ -92,7 +92,7 @@ export function Note({ note, deleteNote, editNote }: NoteProps) {
         }
       }}
     >
-      <Dialog.Trigger className="relative flex flex-col gap-3 overflow-hidden rounded-md bg-slate-700 p-5 outline-none hover:ring hover:ring-slate-600 focus-visible:ring focus-visible:ring-lime-400">
+      <Dialog.Trigger className="relative flex flex-col gap-3 overflow-hidden rounded-4xl bg-slate-700 p-5 outline-none hover:ring hover:ring-slate-600 focus-visible:ring focus-visible:ring-lime-400">
         <span className="text-left text-lg font-medium text-slate-300">
           {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
         </span>
@@ -109,8 +109,8 @@ export function Note({ note, deleteNote, editNote }: NoteProps) {
 
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-black/50" />
-        <Dialog.Content className="fixed inset-0 flex w-full flex-col bg-slate-700 outline-none md:inset-auto md:left-1/2 md:top-1/2 md:h-[60vh] md:max-w-[640px] md:-translate-x-1/2 md:-translate-y-1/2 md:rounded-md">
-          <Dialog.Close className="absolute right-0 top-0 rounded-full bg-slate-800 p-4 text-slate-400 outline-none hover:text-red-400 focus-visible:text-red-400 md:-right-2 md:-top-2">
+        <Dialog.Content className="md:rounded-4xl fixed inset-0 flex w-full flex-col bg-slate-700 outline-none md:inset-auto md:left-1/2 md:top-1/2 md:h-[60vh] md:max-w-[640px] md:-translate-x-1/2 md:-translate-y-1/2">
+          <Dialog.Close className="absolute right-2 top-2 rounded-full bg-slate-800 p-4 text-slate-400 outline-none hover:text-red-400 focus-visible:text-red-400 focus-visible:ring md:-right-2 md:-top-2">
             <X className="size-5" />
           </Dialog.Close>
 
@@ -118,11 +118,11 @@ export function Note({ note, deleteNote, editNote }: NoteProps) {
             <span className="text-lg font-medium text-slate-300">
               {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
             </span>
-            
+
             {shouldShowOnboarding ? (
               <textarea
                 autoFocus
-                className="scrollbar-hide flex-1 resize-none rounded-md bg-slate-600/50 p-2 text-lg leading-6 text-slate-200 outline-none transition focus:shadow-2xl"
+                className="scrollbar-hide flex-1 resize-none rounded-4xl bg-slate-600/50 p-2 text-lg leading-6 text-slate-200 outline-none transition focus:shadow-2xl"
                 defaultValue={contentText}
                 onChange={(event) => {
                   contentText = event.target.value
@@ -137,7 +137,7 @@ export function Note({ note, deleteNote, editNote }: NoteProps) {
             )}
           </div>
 
-          <div className="grid grid-cols-2 gap-x-4 md:gap-x-0">
+          <div className="grid grid-cols-2 gap-x-4 px-2 pb-2 md:gap-x-0">
             {shouldShowOnboarding ? (
               <>
                 <Button
@@ -160,7 +160,7 @@ export function Note({ note, deleteNote, editNote }: NoteProps) {
               <>
                 <Button
                   type="button"
-                  value="Editar nota"
+                  value="editar nota"
                   color="cyan-500"
                   className="md:-translate-x-5"
                   onClick={() => setShouldShowOnboarding(true)}
@@ -168,7 +168,7 @@ export function Note({ note, deleteNote, editNote }: NoteProps) {
 
                 <Button
                   type="button"
-                  value="Apagar nota"
+                  value="apagar nota"
                   color="red-500"
                   className="md:translate-x-5"
                   onClick={() => deleteNote(note.id)}
